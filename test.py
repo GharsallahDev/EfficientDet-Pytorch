80293253d4d0377e7540abe1f1a6e4775842405c
GharsallahDev <118175079+GharsallahDev@users.noreply.github.com>
2024-04-09 08:01:51 +0100

    DD

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-# Assuming the image has already been saved by your script:
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 image_path = '/kaggle/working/EfficientDet/datasets/varroa/test/2017-09-20_19-24-55-mp4-bee_id_4402-44400-1_png_jpg.rf.9da30e6bb3e44c6aa4a6281e4f0b2435.jpg'
 
-# Using IPython.display to show the image
-from IPython.display import Image, display
-display(Image(filename=image_path))
+img = mpimg.imread(image_path)
+plt.imshow(img)
+plt.axis('off')  # Hide axis to focus on the image
+plt.show()
