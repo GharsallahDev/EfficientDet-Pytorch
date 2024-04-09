f9d61d2def32c9c585d2b6b287e102d794824a7a
GharsallahDev <118175079+GharsallahDev@users.noreply.github.com>
2024-04-09 07:59:03 +0100

    DD

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # Assuming the image has already been saved by your script:
-image_path = '/kaggle/working/EfficientDet/test/inferred_2017-09-20_19-24-55-mp4-bee_id_4971-60225-1_png_jpg.rf.1715a8306f2c33b3d843e27f434be8fc.jpg'
+image_path = '/kaggle/working/EfficientDet/datasets/varroa/test/2017-09-20_19-24-55-mp4-bee_id_4402-44400-1_png_jpg.rf.9da30e6bb3e44c6aa4a6281e4f0b2435.jpg'
 
 # Using IPython.display to show the image
 from IPython.display import Image, display
